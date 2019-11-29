481dd511bf34a9ac0459f2c91a9a7b918e14b200
Jannis Jorre <jeyj0.plus@gmail.com>
2019-11-29 13:48:00 +0100

    (feat): basic implementation for equally sized images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use image::io::Reader;
 
-fn generate_diff(image_a: image::DynamicImage, image_b: image::DynamicImage) -> image::RgbaImage {
+fn generate_diff(image_a: image::DynamicImage, image_b: image::DynamicImage) -> (u32, u32, Vec<u8>) {
     let img_a = image_a.to_rgba();
     let img_b = image_b.to_rgba();
 
@@ -8,20 +8,28 @@ fn generate_diff(image_a: image::DynamicImage, image_b: image::DynamicImage) -> 
         panic!("Images have different sizes");
     }
 
-    let img_diff = image::DynamicImage::new_rgba8(img_a.width(), img_a.height()).to_rgba();
+    let mut values: Vec<u8> = vec!();
 
-    let pixel_iter_a = img_a.enumerate_pixels();
-    let pixel_iter_b = img_b.enumerate_pixels();
-    let pixel_iter_diff = img_diff.enumerate_pixels();
+    img_a.pixels().zip(img_b.pixels()).for_each(|(pixel_a, pixel_b)| {
+        values.push(255);
+        values.push(0);
+        values.push(0);
+        if pixel_a != pixel_b {
+            values.push(255);
+        } else {
+            values.push(0);
+        }
+    });
 
-    // iterate over all pixels and store the diff in pixel_iter_diff
+    return (img_a.width(), img_a.height(), values);
 }
 
 fn main() {
     let image_a = Reader::open("./example_a.png").expect("Error reading file a").decode().expect("Error decoding file a");
     let image_b = Reader::open("./example_b.png").expect("Error reading file b").decode().expect("Error decoding file b");
 
-    let image_diff: image::RgbaImage = generate_diff(image_a, image_b);
+    let (width, height, buffer) = generate_diff(image_a, image_b);
 
-    image_diff.save("./example_diff.png").expect("Error saving diff");
+    image::save_buffer_with_format("./example_diff.png", buffer.as_slice(), width, height, image::RGBA(8), image::ImageFormat::PNG).expect("Error saving diff");
 }
+
